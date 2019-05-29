cd0b6f9dcf4d81f63d5ddcb2e105e3dfe95e8542
Slendos <slendos123@gmail.com>
2019-05-29 16:03:06 +0200

    Updates

---
+++ precache-manifest.f62ee4dfacc0ce010ec406a6beed8b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35de8d240553cca3d80af645a325b7c2",
+    "url": "/artifact-gallery/static/media/consumable.35de8d24.png"
+  },
+  {
+    "revision": "526e8ffdc96418351245",
+    "url": "/artifact-gallery/static/js/runtime~main.4295f8bc.js"
+  },
+  {
+    "revision": "300a7d57d6582e0b9567",
+    "url": "/artifact-gallery/static/js/main.8d4e045c.chunk.js"
+  },
+  {
+    "revision": "6ac7f81080c1f67afbee",
+    "url": "/artifact-gallery/static/js/2.216139b9.chunk.js"
+  },
+  {
+    "revision": "300a7d57d6582e0b9567",
+    "url": "/artifact-gallery/static/css/main.42090573.chunk.css"
+  },
+  {
+    "revision": "2bc99e3d675c1b41bd0c643daeaa6d52",
+    "url": "/artifact-gallery/index.html"
+  }
+];
